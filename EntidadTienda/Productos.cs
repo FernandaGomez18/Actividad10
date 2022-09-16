5a7df98c30b3333037d3d9c48af079daae6e63d3
Fernanda Gomez <mmaarryyfer@gmail.com>
2022-09-16 17:21:49 -0500

    Se modifico el Proyecto de Entidad

--- EntidadTienda/Productos.cs
+++ EntidadTienda/Productos.cs
@@ -8,17 +8,19 @@ namespace EntidadTienda
 {
     public class Productos
     {
-        public Productos(int idProducto, string nombre, string descripcion, string precio)
+        public Productos(int idProducto, string nombre, string descripcion, string precio, string proveedor)
         {
             IdProducto = idProducto;
             Nombre = nombre;
             Descripcion = descripcion;
             Precio = precio;
+            Proveedor = proveedor;
         }
 
         public int IdProducto { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public string Precio { get; set; }
+        public string Proveedor { get; set; }
     }
 }
